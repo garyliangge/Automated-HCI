13b98e0e6434e7c90ab98205a4eca6a358872553
Gary Ge <garyliangge@berkeley.edu>
2018-05-02 02:07:40 -0700

    new script saves resized files

---
+++ generate_and_save_labels.py
@@ -0,0 +1,68 @@
+import os
+import json
+import numpy as np
+from os import listdir
+from os.path import isfile, isdir, join
+from PIL import Image
+
+data_path = './filtered_traces/'
+
+
+"""Generate category labels for our screenshots.
+We have 10477 samples in totol.
+"""
+def generate_and_save_labels():
+
+    """Dictionary that maps from screenshot path to category.
+    Categories are:
+    0: No action taken
+    1: Tap
+    2: Swipe
+    """
+    categories = {}
+    count = 0
+
+    app_paths = [join(data_path, app) for app in listdir(data_path) if isdir(join(data_path, app))]
+    for app_path in app_paths:
+
+        #### Used for progress tracking ####
+        count += 1
+        if not count % 100:
+            print("{:.1%}".format(float(count) / len(app_paths)))
+        ####################################
+
+        trace_paths = [join(app_path, trace) for trace in listdir(app_path) if isdir(join(app_path, trace))]
+
+        for trace_path in trace_paths:
+            gesture_path = join(trace_path, "gestures.json")
+            screenshot_dir = join(trace_path, "screenshots/")
+
+            with open(gesture_path) as json_data:
+                d = json.load(json_data)
+                for key in d:
+                    screenshot_path = join(screenshot_dir, key + ".jpg")
+
+
+                    if isfile(screenshot_path): # Verify the screenshot exists
+
+                    	resize_path = join(screenshot_dir, key + "_resize.jpg")
+                    	img = Image.open(screenshot_path).convert('L').resize((400, 400), Image.ANTIALIAS)
+                    	img.save(resize_path)
+
+                        if len(d[key]) == 0:
+                            categories[resize_path] = 0
+                        elif len(d[key]) == 1:
+                            categories[resize_path] = 1
+                        else:
+                            categories[resize_path] = 2
+
+    # Save categories in root-level JSON
+    category_path = "./categories.json"
+    with open(category_path, 'w') as out:
+        out.write(json.dumps(categories))
+        print("Processed {} samples".format(str(count)))
+        print("Labels generated at {}".format(category_path))
+
+
+if __name__ == '__main__':
+    generate_and_save_labels()
