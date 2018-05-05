835ac5b5bbb978ec865c725e5369093011ffa978
Ubuntu <ubuntu@ip-172-31-13-45.us-west-2.compute.internal>
2018-05-05 03:55:59 +0000

    merge confusion matrix code

--- train.py
+++ train.py
@@ -129,11 +129,7 @@ def cnn_model_fn(features, labels, mode):
 	# Configure the Training Op (for TRAIN mode)
 	if mode == tf.estimator.ModeKeys.TRAIN:
 		# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.04)
-<<<<<<< HEAD
-		optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-=======
 		optimizer = tf.train.AdamOptimizer(learning_rate=0.00002)
->>>>>>> confusion_matrix
 		train_op = optimizer.minimize(
 			loss=loss,
 			global_step=tf.train.get_global_step())
