78a2d8d23db8634ec9b2325aff894e063949a91a
Ubuntu <ubuntu@ip-172-31-13-45.us-west-2.compute.internal>
2018-05-06 22:22:36 +0000

    deep net test accuracy now above 40%

--- train_deep.py
+++ train_deep.py
@@ -161,7 +161,7 @@ def cnn_model_fn(features, labels, mode):
 	# Configure the Training Op (for TRAIN mode)
 	if mode == tf.estimator.ModeKeys.TRAIN:
 		# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.04)
-		optimizer = tf.train.AdamOptimizer(learning_rate=0.000015)
+		optimizer = tf.train.AdamOptimizer(learning_rate=0.00001)
 		train_op = optimizer.minimize(
 			loss=loss,
 			global_step=tf.train.get_global_step())
